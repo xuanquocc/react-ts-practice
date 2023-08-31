e027d6adc725ebf95e2a3ec055dea92beaaa2386
xuanquocc <quocntx.21it@vku.udn.vn>
2023-08-31 22:51:11 +0700

    refactor

--- practice-react-ts/vite-project/src/page/detail/index.tsx
+++ practice-react-ts/vite-project/src/page/detail/index.tsx
@@ -16,7 +16,7 @@ const ProductDetailPage: React.FC = () => {
   const { products } = useProviderContext();
 
   const selectedProduct = products.find(
-    (product) => product.id.toString() === productId,
+    (product) => product.id === productId,
   );
 
  
